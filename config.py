17fd716d0c62098aa5e30f19651e10ded8e4355a
Fangfang Sheng <sheng.f@husky.neu.edu>
2020-08-18 18:55:35 -0400

    first commit

--- config.py
+++ config.py
@@ -1,8 +1,7 @@
-# Add your api authorization here.
-twitter_api = {'consumer_key'    : '',
-               'consumer_serect' : '',
-               'access_token'    : '',
-               'access_secret'   : ''}
+twitter_api = {'consumer_key'    : 'l4sWMVVsG5e7Kr46ItcNmaRN4',
+               'consumer_serect' : 'RSZXqSfD7aPlkZHkpDBcsN3OMOgQXSImKo1PPxEOAD6z4bjUhF',
+               'access_token'    : '1106487294280781824-3guA5rUYHEMSiC99JgAPSfcS81g4sd',
+               'access_secret'   : 'uUdEVcsga4ZM5WSuG4GEm00wNeBMYw6AqG6v11YvHrZFd'}
 
 host = 'mongodb://localhost/'    #'twitter_collect_db'
 
